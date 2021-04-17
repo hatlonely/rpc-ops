a12668d88d62f2b991da146b3bc8b2f2e040e1dc
hatlonely.hl <hatlonely.hl@alibaba-inc.com>
2021-04-17 16:29:13 +0800

    update

--- cmd/main.go
+++ cmd/main.go
@@ -2,8 +2,39 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/hatlonely/go-kit/flag"
+	"github.com/hatlonely/go-kit/logger"
+	"github.com/hatlonely/go-kit/refx"
+	"github.com/hatlonely/go-kit/rpcx"
+
+	"github.com/hatlonely/rpc-ops/internal/service"
 )
 
+var Version string
+
+type Options struct {
+	flag.Options
+
+	Service     service.Options
+	GrpcGateway rpcx.GrpcGatewayOptions
+
+	Logger struct {
+		Info logger.Options
+		Grpc logger.Options
+	}
+}
+
 func main() {
-	fmt.Println("hello world")
+	var options Options
+	refx.Must(flag.Struct(&options, refx.WithCamelName()))
+	refx.Must(flag.Parse(flag.WithJsonVal()))
+	if options.Help {
+		fmt.Println(flag.Usage())
+		return
+	}
+	if options.Version {
+		fmt.Println(Version)
+		return
+	}
 }
