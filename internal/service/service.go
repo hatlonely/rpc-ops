374a3d1baf470af2f06e51dc8437f458808ee3c0
hatlonely.hl <hatlonely.hl@alibaba-inc.com>
2021-04-17 16:05:49 +0800

    update

---
+++ internal/service/service.go
@@ -0,0 +1,29 @@
+package service
+
+import (
+	"github.com/hatlonely/go-kit/refx"
+	"github.com/pkg/errors"
+
+	"github.com/hatlonely/rpc-ops/internal/storage"
+)
+
+type Options struct {
+	Storage storage.OpsOptions
+}
+
+func NewServiceWithOptions(options *Options, opts ...refx.Option) (*Service, error) {
+	storage, err := storage.NewOpsStorageWithOptions(&options.Storage, opts...)
+	if err != nil {
+		return nil, errors.WithMessage(err, "storage.NewOpsStorageWithOptions failed")
+	}
+
+	return &Service{
+		storage: storage,
+		options: options,
+	}, nil
+}
+
+type Service struct {
+	storage *storage.OpsStorage
+	options *Options
+}
