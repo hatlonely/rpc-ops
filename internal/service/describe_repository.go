281f55f849e2827489fa4bbb54a094b6a087b00d
hatlonely.hl <hatlonely.hl@alibaba-inc.com>
2021-04-18 21:40:23 +0800

    update

--- internal/service/describe_repository.go
+++ internal/service/describe_repository.go
@@ -1,1 +1,11 @@
 package service
+
+import (
+	"context"
+
+	"github.com/hatlonely/rpc-ops/api/gen/go/api"
+)
+
+func (s *Service) DescribeRepository(ctx context.Context, req *api.DescribeRepositoryReq) (*api.DescribeRepositoryRes, error) {
+	return nil, nil
+}
--- internal/service/run_ops.go
+++ internal/service/run_ops.go
@@ -1,1 +1,11 @@
 package service
+
+import (
+	"context"
+
+	"github.com/hatlonely/rpc-ops/api/gen/go/api"
+)
+
+func (s *Service) RunOps(ctx context.Context, req *api.RunOpsReq) (*api.RunOpsRes, error) {
+	return nil, nil
+}
