84b6ef69fcf641397fef26e1d252756205517e21
hatlonely.hl <hatlonely.hl@alibaba-inc.com>
2021-04-20 00:25:12 +0800

    update

--- internal/ops/executor.go
+++ internal/ops/executor.go
@@ -2,6 +2,7 @@ package ops
 
 import (
 	"context"
+	"sync"
 
 	"github.com/hatlonely/go-kit/logger"
 	gops "github.com/hatlonely/go-kit/ops"
@@ -13,24 +14,55 @@ type ExecutorOptions struct {
 }
 
 func NewExecutorWithOptions(manager *Manager, options *ExecutorOptions) (*Executor, error) {
+	ctx, cancel := context.WithCancel(context.Background())
 
 	return &Executor{
 		manager: manager,
 		infoLog: logger.NewStdoutJsonLogger(),
 		workLog: logger.NewStdoutJsonLogger(),
+		ctx:     ctx,
+		cancel:  cancel,
 	}, nil
 }
 
 type Executor struct {
+	options *ExecutorOptions
 	manager *Manager
 
 	infoLog *logger.Logger
 	workLog *logger.Logger
+
+	ctx    context.Context
+	cancel context.CancelFunc
+	wg     sync.WaitGroup
 }
 
 func (e *Executor) SetLogger(infoLog *logger.Logger, workLog *logger.Logger) {
 	e.infoLog = infoLog
 	e.workLog = workLog
+}
+
+func (e *Executor) Run() {
+	for i := 0; i < e.options.WorkerNum; i++ {
+		e.wg.Add(1)
+		go func() {
+		out:
+			for {
+				select {
+				case <-e.ctx.Done():
+					break out
+				default:
+				}
+				_ = e.work(e.ctx)
+			}
+			e.wg.Done()
+		}()
+	}
+}
+
+func (e *Executor) Stop() {
+	e.cancel()
+	e.wg.Wait()
 }
 
 func (e *Executor) work(ctx context.Context) error {
