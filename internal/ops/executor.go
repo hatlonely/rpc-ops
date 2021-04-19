0ffe1bbfcbf9476f13edf9635c05999665a3ae9d
hatlonely.hl <hatlonely.hl@alibaba-inc.com>
2021-04-19 23:11:49 +0800

    update

--- internal/ops/executor.go
+++ internal/ops/executor.go
@@ -1,7 +1,23 @@
 package ops
 
+import "github.com/hatlonely/go-kit/logger"
+
 type ExecutorOptions struct {
 }
 
+func NewExecutorWithOptions(options *ExecutorOptions) (*Executor, error) {
+	return &Executor{
+		infoLog: logger.NewStdoutJsonLogger(),
+		workLog: logger.NewStdoutJsonLogger(),
+	}, nil
+}
+
 type Executor struct {
+	infoLog *logger.Logger
+	workLog *logger.Logger
+}
+
+func (e *Executor) SetLogger(infoLog *logger.Logger, workLog *logger.Logger) {
+	e.infoLog = infoLog
+	e.workLog = workLog
 }
