43a325d388ce89e1ba0a9e03a03dc4ba7d9cfe0d
hatlonely.hl <hatlonely.hl@alibaba-inc.com>
2021-09-30 10:26:37 +0800

    update

--- internal/executor/ops_executor.go
+++ internal/executor/ops_executor.go
@@ -1,6 +1,11 @@
 package executor
 
-import "github.com/hatlonely/go-kit/logger"
+import (
+	"github.com/hatlonely/go-kit/logger"
+	"github.com/pkg/errors"
+
+	"github.com/hatlonely/rpc-ops/internal/ops"
+)
 
 type OpsOptions struct {
 	Executor Options
@@ -9,23 +14,35 @@ type OpsOptions struct {
 	ConsumerNum int `dft:"10"`
 }
 
-func NewOpsExecutorWithOptions(options *OpsOptions, infoLogger, execLogger *logger.Logger) *OpsExecutor {
+func NewOpsExecutorWithOptions(options *OpsOptions, manager *ops.Manager, infoLogger, execLogger *logger.Logger) *OpsExecutor {
 	executor := NewExecutorWithOptions(&options.Executor, infoLogger, execLogger)
-
-	executor.SetProducerFunc(options.ProducerNum, func() (interface{}, error) {
-		return nil, nil
-	})
-	executor.SetConsumerFunc(options.ConsumerNum, func(i interface{}) error {
-		return nil
-	})
-
-	return &OpsExecutor{
+	oe := &OpsExecutor{
 		executor: executor,
+		manager:  manager,
 	}
+
+	executor.SetProducerFunc(options.ProducerNum, oe.Produce)
+	executor.SetConsumerFunc(options.ConsumerNum, oe.Consume)
+
+	return oe
 }
 
 type OpsExecutor struct {
 	executor *Executor
+	manager  *ops.Manager
+}
+
+func (e *OpsExecutor) Produce() (interface{}, error) {
+	job, err := e.manager.FindOneWaitingJob(e.executor.ctx)
+	if err != nil {
+		return nil, errors.WithMessage(err, "m.manager.FindOneWaitingJob failed")
+	}
+
+	return job, nil
+}
+
+func (e *OpsExecutor) Consume(interface{}) error {
+	return nil
 }
 
 func (e *OpsExecutor) Run() {
